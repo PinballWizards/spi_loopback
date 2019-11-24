d44ebeb49ec148e1ebbd51701993ffb0aea13b37
Will Tekulve <tekulve.will@gmail.com>
2019-11-24 03:33:28 -0500

    create new spi master

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@
 
 extern crate cortex_m;
 extern crate cortex_m_semihosting;
+extern crate embedded_hal;
 extern crate feather_m0 as hal;
 extern crate panic_halt;
 
 use hal::clock::GenericClockController;
-use hal::delay::Delay;
 use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
 
 use hal::entry;
+
+mod spi;
 
 #[entry]
 fn main() -> ! {
@@ -23,8 +25,25 @@ fn main() -> ! {
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
+    let mut pins = hal::Pins::new(peripherals.PORT);
+    let d13 = pins.d13.into_open_drain_output(&mut pins.port);
 
-    loop {
-        
-    }
+    let raw_spi3_master = spi::spi_master3(
+        &mut clocks,
+        3u32.mhz(),
+        peripherals.SERCOM3,
+        &mut peripherals.PM,
+        pins.d12,
+        pins.d10,
+        pins.d11,
+        &mut pins.port,
+    );
+
+    let data = &[5];
+
+    let mut spi3_master = spi::SPI::new(raw_spi3_master, d13);
+
+    spi3_master.send(data).unwrap();
+
+    loop {}
 }
---
+++ src/spi.rs
@@ -0,0 +1,71 @@
+use core::iter::Iterator;
+
+use nb;
+
+use hal::clock::GenericClockController;
+use hal::gpio::*;
+use hal::pac::{PM, SERCOM3};
+use hal::sercom::*;
+use hal::time::*;
+
+use embedded_hal::digital::v2::OutputPin;
+
+pub struct SPI<T, SS> {
+    spi_master: T,
+    slave_select: SS,
+}
+
+impl<T, SS> SPI<T, SS>
+where
+    T: embedded_hal::spi::FullDuplex<u8>,
+    SS: OutputPin,
+    <SS as OutputPin>::Error : core::fmt::Debug
+{
+    pub fn new(spi_master: T, mut slave_select: SS) -> SPI<T, SS> {
+        slave_select.set_low().expect("could not control slave select");
+        Self {
+            spi_master,
+            slave_select
+        }
+    }
+
+    pub fn read(&mut self) -> nb::Result<u8, T::Error> {
+        let ret = self.spi_master.read();
+        ret
+    }
+
+    pub fn send(&mut self, data: &[u8]) -> nb::Result<(), T::Error> {
+        data.iter().map(|v| {
+            self.spi_master.send(*v)
+        }).collect()
+    }
+
+    pub fn free(mut self) -> (T, SS) {
+        self.slave_select.set_high().expect("could not control slave select");
+        (self.spi_master, self.slave_select)
+    }
+}
+
+pub fn spi_master3<F: Into<Hertz>>(
+    clocks: &mut GenericClockController,
+    bus_speed: F,
+    sercom3: SERCOM3,
+    pm: &mut PM,
+    sck: Pa19<Input<Floating>>,
+    mosi: Pa18<Input<Floating>>,
+    miso: Pa16<Input<Floating>>,
+    port: &mut Port,
+) -> SPIMaster3<Sercom3Pad0<Pa16<PfD>>, Sercom3Pad2<Pa18<PfD>>, Sercom3Pad3<Pa19<PfD>>> {
+    let gclk0 = clocks.gclk0();
+    SPIMaster3::new(
+        &clocks.sercom3_core(&gclk0).unwrap(),
+        bus_speed.into(),
+        embedded_hal::spi::Mode {
+            phase: embedded_hal::spi::Phase::CaptureOnFirstTransition,
+            polarity: embedded_hal::spi::Polarity::IdleLow,
+        },
+        sercom3,
+        pm,
+        (miso.into_pad(port), mosi.into_pad(port), sck.into_pad(port)),
+    )
+}
